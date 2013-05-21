2348a0eedfe229fd24fcc594b1cf683e5ca97af6
Eirik Sand <eiriksand@gmail.com>
2013-05-21 06:48:31 +0200

    Changed version

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject clojure-config "1.0.0"
+(defproject clojure-config "1.0"
   :description "A simple set of functions to handle configuration"
   :url "http://github.com/ezand/clojure-config"
   :java-source-paths ["src/main/java"]
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [me.raynes/fs "1.4.2"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
